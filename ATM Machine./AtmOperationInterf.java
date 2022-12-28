2194dfc5b18022262663ab51f1be72035dda5c70
adarsh3999 <79866619+adarsh3999@users.noreply.github.com>
2022-12-28 19:35:29 +0530

    Add files via upload

---
+++ ATM Machine./AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package dec28_2022;
+
+public interface AtmOperationInterf {
+    public void viewBalance();
+    public void withdrawAmount(double withdrawAmount);
+    public void depositAmount(double depositAmount );
+    public  void viewMiniStatement();
+}
